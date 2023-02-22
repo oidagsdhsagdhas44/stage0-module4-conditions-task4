5dea0e706c07ff046655fa6be9f85611a239a8f4
oidagsdhsagdhas44 <tornike.buchukuri@gau.edu.ge>
2023-02-23 00:22:20 +0400

    module4 task4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int greaterOfTwo = first>second ? first : second;
+        int greatest = third>greaterOfTwo ? third : greaterOfTwo;
+        System.out.println(greatest);
     }
 }
